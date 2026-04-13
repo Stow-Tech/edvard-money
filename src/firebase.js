a4d7a3f55b2bc67afd26c7c288b14209358e9c7d
Ino <eddstownavy@gmail.com>
2026-04-12 19:42:12 -0600

    move firebase config to env

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDrNMu1RRmEW3tv05m_OC8f9PJRhdUL1RY",
-  authDomain: "edvard-money.firebaseapp.com",
-  projectId: "edvard-money",
-  storageBucket: "edvard-money.firebasestorage.app",
-  messagingSenderId: "916445732739",
-  appId: "1:916445732739:web:da460be56bc310e2ad20b6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
