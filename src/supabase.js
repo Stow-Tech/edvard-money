2091e94970f9ac30a2c3eeff6c572b0260940935
Edvard <edvard@Edvards-MacBook-Air.local>
2026-05-02 15:48:46 -0600

    fix: use env vars for Supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://rciqjzagiekcxqspwsjh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJjaXFqemFnaWVrY3hxc3B3c2poIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMDE4MDQsImV4cCI6MjA5MDY3NzgwNH0.fD_zJgZbcWAaeYDY0lp7sIEjiAXEGlhFLha1DWVD4DA';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
